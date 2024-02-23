8f6af468355c349007339b4b921d9ef258a810b0
Mohana-Kulkarni <mpkulkarni117@gmail.com>
2024-02-23 11:29:49 +0530

    OpenAPI documentation added

--- src/main/java/com/example/ssisystem/SsiSystemApplication.java
+++ src/main/java/com/example/ssisystem/SsiSystemApplication.java
@@ -1,10 +1,12 @@
 package com.example.ssisystem;
 
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+@OpenAPI30
+@SpringBootApplication(scanBasePackages = "com.example.ssisystem")
 public class SsiSystemApplication {
 
 	public static void main(String[] args) {
