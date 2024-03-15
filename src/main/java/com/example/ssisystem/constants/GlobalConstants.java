67090a2972883e256fc9b8498504a408c26e068a
Mohana-Kulkarni <mpkulkarni117@gmail.com>
2024-03-15 12:07:41 +0530

    Global Constants file added

---
+++ src/main/java/com/example/ssisystem/constants/GlobalConstants.java
@@ -0,0 +1,30 @@
+package com.example.concertsystem.constants;
+
+public final class GlobalConstants {
+
+    private GlobalConstants() {
+        // restrict instantiation
+    }
+
+
+    public static final String  STATUS_201 = "201";
+    public static final String STATUS_409 = "409";
+    public static final String STATUS_451 = "451";
+    public static final String  MESSAGE_201_Event = "Event Created successfully";
+    public static final String  MESSAGE_201_User = "User Created successfully";
+    public static final String  MESSAGE_201_Venue = "Venue Created successfully";
+    public static final String  MESSAGE_201_Place = "Place Created successfully";
+    public static final String  MESSAGE_201_Organiser = "Organiser Created successfully";
+    public static final String  MESSAGE_201_Ticket = "Ticket Created successfully";
+    public static final String  STATUS_200 = "200";
+    public static final String  MESSAGE_200 = "Request processed successfully";
+    public static final String  STATUS_417 = "417";
+    public static final String  MESSAGE_417_POST= "Post operation failed. Please try again or contact Dev team";
+    public static final String  MESSAGE_417_UPDATE= "Update operation failed. Please try again or contact Dev team";
+    public static final String  MESSAGE_417_DELETE= "Delete operation failed. Please try again or contact Dev team";
+    public static final String  STATUS_500 = "500";
+    public static final String  MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
+    public static final String  MESSAGE_451 = "The resource has never existed";
+
+
+}
