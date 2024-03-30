c12eac533c26e5f4d62e7003c0f9ae05e14986d6
Mohana-Kulkarni <mpkulkarni117@gmail.com>
2024-03-30 13:27:43 +0530

    Did List Request body added

--- src/main/java/com/example/ssisystem/requests/DidRequest.java
+++ src/main/java/com/example/ssisystem/requests/DidRequest.java
@@ -1,2 +1,22 @@
-package com.example.ssisystem.requests;public class DidRequest {
+package com.example.ssisystem.requests;
+
+import java.util.List;
+
+public class DidRequest {
+    private List<String> issuersDid;
+
+    public DidRequest() {
+    }
+
+    public DidRequest(List<String> issuersDid) {
+        this.issuersDid = issuersDid;
+    }
+
+    public List<String> getIssuersDid() {
+        return issuersDid;
+    }
+
+    public void setIssuersDid(List<String> issuersDid) {
+        this.issuersDid = issuersDid;
+    }
 }
